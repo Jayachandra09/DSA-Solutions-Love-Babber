7454123f5cc72fe028f03d53f941445defc10fde
Jayachandra09 <103034641+Jayachandra09@users.noreply.github.com>
2024-07-16 10:39:29 +0530

    Create palindromic_array.py

---
+++ palindromic_array.py
@@ -0,0 +1,24 @@
+def PalinArray(arr):
+    # Code here
+    count = 0
+    for i in arr:
+        remainder = 0
+        reverse = 0
+        temp = i 
+        while(temp>0):
+            remainder = temp%10
+            reverse = reverse*10+remainder
+            temp = temp//10
+        if i == reverse:
+            count+=1
+    if count == len(arr):
+        return True
+    else:
+        return False
+
+# we can solve this by using string also we need to traverse from the back like str[::-1] ---if the string reverse is equal to string we can increse the count or we can directly return false in the else statement.
+
+# Driver Code
+arr = [111, 222, 333, 444, 555]
+result = PalinArray(arr)
+print(result)
