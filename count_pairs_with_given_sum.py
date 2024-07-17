7e403aad54eb0a35f14215a723184309199e4044
Jayachandra09 <103034641+Jayachandra09@users.noreply.github.com>
2024-07-17 13:16:55 +0530

    Create count_pairs_with_given_sum.py

---
+++ count_pairs_with_given_sum.py
@@ -0,0 +1,20 @@
+def PairsCount(arr, target):
+  count = 0
+  dic = {}
+  for i in arr:
+    if target-i in dic:
+    # 
+      count+=dic[target-i]
+    if i in dic:
+      dic[i]+=1
+    else:
+      dic[i]=1
+  return count
+
+#Driver code
+arr = [1,5,7,1]
+target = 6
+result = PairsCount(arr, target)
+print(result)
+
+#Problem Link : https://www.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1
